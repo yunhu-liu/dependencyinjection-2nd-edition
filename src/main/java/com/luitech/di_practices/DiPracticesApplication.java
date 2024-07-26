3d9f70f1ee688fbd6b47dfc8f8a811bc51d936f2
yunhu <yunhu.liu@outlook.com>
2024-07-27 10:06:14 +1200

    Add spring boot project to the repository

---
+++ src/main/java/com/luitech/di_practices/DiPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.luitech.di_practices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiPracticesApplication.class, args);
+	}
+
+}
